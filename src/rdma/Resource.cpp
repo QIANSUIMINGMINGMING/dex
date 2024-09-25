30b347be89ebc3f12f9921a8852fb3f50dc4d23b
Muxi <1105989949@qq.com>
2024-09-25 12:15:33 +0000

    update

--- src/rdma/Resource.cpp
+++ src/rdma/Resource.cpp
@@ -26,7 +26,7 @@ bool createContext(RdmaContext *context, uint8_t port, int gidIndex,
 
   for (int i = 0; i < devicesNum; ++i) {
     // printf("Device %d: %s\n", i, ibv_get_device_name(deviceList[i]));
-    if (ibv_get_device_name(deviceList[i])[5] == '0') {
+    if (ibv_get_device_name(deviceList[i])[5] == '2') {
       // if (ibv_get_device_name(deviceList[i]) == "mlx5_1") {
       //      printf("Device %d: %s\n", i, ibv_get_device_name(deviceList[i]));
       devIndex = i;
