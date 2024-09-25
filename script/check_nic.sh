0298330697d704568985dbd02a58d9c680d93411
Muxi <1105989949@qq.com>
2024-09-25 02:19:22 +0000

    update

--- script/check_nic.sh
+++ script/check_nic.sh
@@ -2,7 +2,7 @@
 
 
 net_name=`ibdev2netdev | awk '{print $5}'`
-ip=`ifconfig 2>>/dev/null | grep 10.0.2. | awk '{print $2}'`
+ip=`ifconfig 2>>/dev/null | grep 10.16. | awk '{print $2}'`
 for net in $net_name; do
     str1=`ibdev2netdev | grep $net`
     str2=`cat /sys/class/net/$net/device/numa_node`
