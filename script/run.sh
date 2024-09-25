eb14b0337ac1c261e293a2c99f7b550bea70eb93
Muxi <1105989949@qq.com>
2024-09-25 12:05:10 +0000

    update

--- script/run.sh
+++ script/run.sh
@@ -51,7 +51,7 @@ do
                     sleep 2
                 fi
                 # ./restartMemc.sh
-                sudo ./newbench $nodenum ${read[$op]} ${insert[$op]} ${update[$op]} ${delete[$op]} ${range[$op]} ${threads[$t]} ${mem_threads[1]} ${cache[3]} $uni ${zipf[0]} $bulk $warmup $runnum $correct $timebase $early $idx $rpc $admit $tune 36
+                sudo ../build/newbench $nodenum ${read[$op]} ${insert[$op]} ${update[$op]} ${delete[$op]} ${range[$op]} ${threads[$t]} ${mem_threads[1]} ${cache[3]} $uni ${zipf[0]} $bulk $warmup $runnum $correct $timebase $early $idx $rpc $admit $tune 36
                 sleep 2
             done
         done
