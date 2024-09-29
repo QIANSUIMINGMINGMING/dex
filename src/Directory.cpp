d5d7279326d3be2af39461116047a6b25c9afbb9
Muxi <1105989949@qq.com>
2024-09-29 03:11:41 +0000

    update

--- src/Directory.cpp
+++ src/Directory.cpp
@@ -63,6 +63,9 @@ void Directory::dirThread() {
 }
 
 void Directory::process_message(const RawMessage *m) {
+  if (m->addr.nodeID != 3) {
+    printf("m node ID %llu", m->addr.nodeID);
+  }
   assert(m->addr.nodeID == 3);
   RawMessage *send = nullptr;
   switch (m->type) {
