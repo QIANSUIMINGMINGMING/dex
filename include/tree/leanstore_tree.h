1f947712400384d105ffac4ffc833e012334c34a
Muxi <1105989949@qq.com>
2024-09-29 02:09:51 +0000

    exp update

--- include/tree/leanstore_tree.h
+++ include/tree/leanstore_tree.h
@@ -579,10 +579,10 @@ public:
         assert(ret == true);
       }
     } else {
-      if (node.nodeID == node_id) {
-        std::cout << "Existing ID = " << node.nodeID << std::endl;
-        std::cout << "New ID = " << node_id << std::endl;
-      } 
+      // if (node.nodeID == node_id) {
+      //   std::cout << "Existing ID = " << node.nodeID << std::endl;
+      //   std::cout << "New ID = " << node_id << std::endl;
+      // } 
       // assert(node.nodeID != node_id);
       // last-level node movement
       auto new_node = allocate_node(node_id);
