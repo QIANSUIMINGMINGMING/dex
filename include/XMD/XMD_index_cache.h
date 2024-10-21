64c4c70cfa9e9f8f71978b0d0f4620f6f0108806
Muxi <1105989949@qq.com>
2024-10-21 18:21:18 +0000

    update

--- include/XMD/XMD_index_cache.h
+++ include/XMD/XMD_index_cache.h
@@ -584,8 +584,8 @@ class CacheManager {
         0, 9999);
     auto idx = distribution(*generator);
     uint64_t admission_idx = 10000 * admission_rate_;
-    if (cachepush::cache_allocator::get_state() == 1 && idx >= admission_idx) {
-      // if (true) { 
+    // if (cachepush::cache_allocator::get_state() == 1 && idx >= admission_idx) {
+      if (true) { 
       // Just read from remote and return to the application
       int ret = 1;
       switch (rpc_type) {
