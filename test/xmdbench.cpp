c26597b20c1009f80b8c42c94b55c57d9ace1acd
Muxi <1105989949@qq.com>
2024-11-18 03:13:46 +0000

    update

--- test/xmdbench.cpp
+++ test/xmdbench.cpp
@@ -1151,13 +1151,13 @@ int main(int argc, char *argv[]) {
       //   total_max_time = std::max_element(total_time, total_time + k)
       // }
       total_max_time = *std::max_element(total_time, total_time + kThreadCount);
-      total_cluster_max_time = dsm->max_total(total_max_time, CNodeCount);
+      // total_cluster_max_time = dsm->max_total(total_max_time, CNodeCount);
       std::cout << "XMD node max time: " << total_max_time;
       std::cout << "XMD cluster max time: " << total_cluster_max_time;
 
       uint64_t XMDsetting_node_throughput =
           execute_op.load() /
-          (static_cast<double>(total_cluster_max_time) / std::pow(10, 6));
+          (static_cast<double>(total_max_time) / std::pow(10, 6));
       uint64_t XMDsetting_cluster_throughput =
           dsm->sum_total(XMDsetting_node_throughput, CNodeCount, false);
       
