645e119fdef6871d156139b88b61a9c951d25a88
Muxi Yan <1105989949@qq.com>
2024-12-03 19:10:18 +0000

    update

--- include/XMD/BatchForest.h
+++ include/XMD/BatchForest.h
@@ -68,7 +68,7 @@ class BatchForest : public tree_api<T, P> {
     // std::cout << "lookup" << key << std::endl;
     XMD::BatchBTree *shard_tree = btrees[shard_id];
     ret = shard_tree->search(key, value);
-    if (ret) {
+    if (ret&& rand() % 2 ==0) {
       request_cache_->insert_no_TS(key, value);
     }
     return ret;
